74ffc92b91ad9121bd1dc4915f986cb78de919e0
Rodrigo A. Magalhaes <rodrigo.a.magalhaes@hotmail.com>
2021-10-04 22:13:34 -0300

    fix: otimizacao da aplicacao

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -1,6 +1,6 @@
-import { Star, Clock } from 'react-feather';
-
-import '../styles/movie-card.scss';
+import { memo } from "react";
+import { Clock, Star } from "react-feather";
+import "../styles/movie-card.scss";
 
 interface MovieCardProps {
   title: string;
@@ -9,13 +9,10 @@ interface MovieCardProps {
   runtime: string;
 }
 
-export function MovieCard(props: MovieCardProps) {
+function MovieCardComponent(props: MovieCardProps) {
   return (
     <div className="movie-card">
-      <img
-        src={props.poster}
-        alt={props.title}
-      />
+      <img src={props.poster} alt={props.title} />
 
       <div>
         <div className="movie-info">
@@ -32,5 +29,9 @@ export function MovieCard(props: MovieCardProps) {
         </div>
       </div>
     </div>
-  )
+  );
 }
+
+export const MovieCard = memo(MovieCardComponent, (prevProps, nextProps) => {
+  return Object.is(prevProps.title, nextProps.title);
+});
